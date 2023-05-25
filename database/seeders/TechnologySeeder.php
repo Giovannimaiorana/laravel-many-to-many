0b16a511b3453a8afc155cb9666919134646f7c5
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-25 16:00:18 +0200

    effettuata seed

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -1,19 +1,23 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+
     public function run()
     {
-        //
+        $technologies = ['Php','Laravel','Js','Vue','React','Css','Html'];
+        foreach ($technologies as $technology){
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology, '-');
+            $newTechnology->save();
+
+        }
     }
 }
